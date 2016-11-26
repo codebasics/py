d56d563239fd1e64a9233b9fe1800c0c5036260b
learnp <learnpythonlanguage@gmail.com>
2016-11-26 17:44:46 -0500

    Fixed a bug in test.py

--- Basics/13_read_write_file.py
+++ Basics/13_read_write_file.py
@@ -1,4 +1,34 @@
-with open("C:\\data\\funny.txt","r") as f:
-    print(f.read())
+def count_num_in_file(file_path, num):
+    count = 0
+    with open(file_path,"r") as f:
+        for line in f.readlines():
+            tokens = line.split(",")
+            count += count_num_in_tokens(tokens, num)
 
-print(f.closed)
+    return count
+
+def count_num_in_tokens(tokens, num):
+    count = 0
+    for token in tokens:
+        if num == int(token):
+            count+=1
+    return count
+
+
+def sum_numbers(file_path):
+    output_lines = []
+    with open(file_path,"r") as f:
+        for line in f.readlines():
+            tokens = line.split(",")
+            total = sum_tokens(tokens)
+            output_lines.append("sum: " + str(total) + " | " + line)
+    with open(file_path,"w") as f:
+        f.writelines(output_lines)
+
+def sum_tokens(tokens):
+    sum = 0
+    for token in tokens:
+        sum += int(token)
+    return sum
+
+sum_numbers("C:\\Code\\Py\\Basics\\input.txt")
