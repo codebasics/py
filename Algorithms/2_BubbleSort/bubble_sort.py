678738e3edd25cb6bf9f9f763710b3b29026e0eb
codebasics <learnpythonlanguage@gmail.com>
2020-09-17 18:26:46 -0400

    bubble sort

---
+++ Algorithms/2_BubbleSort/bubble_sort.py
@@ -0,0 +1,25 @@
+
+# you can use this to sort strings too
+def bubble_sort(elements):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            if elements[j] > elements[j+1]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+
+if __name__ == '__main__':
+    elements = [5,9,2,1,67,34,88,34]
+    elements = [1,2,3,4,2]
+    elements = ["mona", "dhaval", "aamir", "tina", "chang"]
+
+    bubble_sort(elements)
+    print(elements)
---
+++ Algorithms/2_BubbleSort/bubble_sort_exercise_solution.py
@@ -0,0 +1,28 @@
+# you can use this to sort strings too
+def bubble_sort(elements, key=None):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            a = elements[j][key]
+            b = elements[j+1][key]
+            if a > b:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
+
+if __name__ == '__main__':
+    elements = [
+        { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+        { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+        { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+        { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    ]
+
+    bubble_sort(elements, key='transaction_amount')
+    print(elements)
