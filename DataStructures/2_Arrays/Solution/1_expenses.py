f4f46b029023c60a3442989a37f58a532cca8575
Muhammad TALHA <65563164+Talha1818@users.noreply.github.com>
2020-10-01 11:32:46 +0500

    Update 1_expenses.py

--- DataStructures/2_Arrays/Solution/1_expenses.py
+++ DataStructures/2_Arrays/Solution/1_expenses.py
@@ -16,19 +16,42 @@
 # based on this
 
 exp = [2200,2350,2600,2130,2190]
+#we can also create a list like this
+expenses = [['january',2200],['February',2350],['March',2600],['April',2130],['May',2190]]
 
 # 1. In Feb, how many dollars you spent extra compare to January?
 print("In feb this much extra was spent compared to jan:",exp[1]-exp[0]) # 150
 
+comp = expenses[1][1] - expenses[0][1]
+print("Extra Spent as compare to January : ",comp) #150
+
 # 2. Find out your total expense in first quarter (first three months) of the year
 print("Expense for first quarter:",exp[0]+exp[1]+exp[2]) # 7150
+
+total = 0
+for i in range(3):
+    total+=expenses[i][1]
+print("Total expenses of first 3 month : ",total)  #7150
 
 # 3. Find out if you spent exactly 2000 dollars in any month
 print("Did I spent 2000$ in any month? ", 2000 in exp) # False
 
+flag = False
+for i in range(len(expenses)):
+    if expenses[i][1]==2000:
+        print("This month exactly 2000 : ",expenses[i][0])
+        flag = True
+
+if flag==False:
+    print("Not any month which exactly Spent 2000$ ") #Not any month which exactly Spent 2000$
+
+
 # 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
 exp.append(1980)
 print("Expenses at the end of June:",exp) # [2200, 2350, 2600, 2130, 2190, 1980]
+
+print("June added in expenses list : ",expenses) #June added in expenses :  [['january', 2200], ['February', 2350], ['March', 2600], ['April', 2130], ['May', 2190], ['June', 1980]] 
+
 
 # 5. You returned an item that you bought in a month of April and
 # got a refund of 200$. Make a correction to your monthly expense list
