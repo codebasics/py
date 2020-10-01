1754aa82c7a9184f5b98a12c6d760033db9931b2
Muhammad TALHA <65563164+Talha1818@users.noreply.github.com>
2020-10-01 11:46:54 +0500

    Update 3_odd_even_numbers.py

--- DataStructures/2_Arrays/Solution/3_odd_even_numbers.py
+++ DataStructures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -7,3 +7,9 @@ for i in range(max):
         odd_numbers.append(i)
 
 print("Odd numbers: ",odd_numbers)
+
+
+#we can also used list_comprehension to find the odd numbers
+n = int(input("Enter a max number:"))
+odd = [i for i in range(n) if i%2==1]
+print("Odd Numbers : ",odd)
