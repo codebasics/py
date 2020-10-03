3a34def4e50e5247524e8cfa658905715c7331fd
Jeevesh Joshi <64859843+Jeevesh-Joshi@users.noreply.github.com>
2020-10-03 15:29:07 +0530

    Update multiprocessing_pool.py
    Comparable code for serial processing v/s pool.

--- Multiprocessing/multiprocessing_pool.py
+++ Multiprocessing/multiprocessing_pool.py
@@ -1,11 +1,31 @@
-from multiprocessing import Pool
+# Multiprocessing Pool
 
+from multiprocessing import Pool
+import time
 
 def f(n):
-    return n*n
+    sum = 0
+    for i in n:
+        sum += i
+    return sum
 
 if __name__ == "__main__":
+    n = 10000000
+
+    t1 = time.time()
     p = Pool(processes=3)
-    result = p.map(f,[1,2,3,4,5])
-    for n in result:
-        print(n)
+
+    # map(function_name,iterable data_variable)
+    result = p.map(f,[range(n)])
+    p.close()
+    p.join()
+
+    print('Pool took time =',time.time()-t1)
+
+    t2 = time.time()
+    sum = 0
+    for i in range(n):
+        sum += i
+    
+    print('Serial Processing took time =',time.time()-t2)
+
