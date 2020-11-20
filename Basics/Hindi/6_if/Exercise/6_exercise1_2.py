40e74e78f3b228a467643e45bbfaa76c43529bfc
Mayank Motiyani <72161636+Monty-tech@users.noreply.github.com>
2020-11-20 18:12:48 +0530

    last elif statement need changes

--- Basics/Hindi/6_if/Exercise/6_exercise1_2.py
+++ Basics/Hindi/6_if/Exercise/6_exercise1_2.py
@@ -18,7 +18,7 @@ if city1 in india and city2 in india:
     print("Both cities are in india")
 elif city1 in pakistan and city2 in pakistan:
     print("Both cities are in pakistan")
-elif city1 in bangladesh and bangladesh in india:
+elif city1 in bangladesh and city2 in bangladesh:
     print("Both cities are in bangladesh")
 else:
     print("They don't belong to same country")
