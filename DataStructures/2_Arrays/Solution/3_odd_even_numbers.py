89beaab08f67c169881142a5b6785b72d54c1f18
Vikas Kumar <54888022+vikasit12@users.noreply.github.com>
2021-01-08 09:22:15 +0530

    Update 3_odd_even_numbers.py
    If max is an odd number that will be excluded due to range function hence (max+1)
    Rather than manual checking of odd we can use steps of range to generate the same

--- DataStructures/2_Arrays/Solution/3_odd_even_numbers.py
+++ DataStructures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -2,8 +2,7 @@ max = int(input("Enter max number: "))
 
 odd_numbers = []
 
-for i in range(max):
-    if i%2 == 1:
-        odd_numbers.append(i)
+for i in range(1,max+1,2):
+    odd_numbers.append(i)
 
 print("Odd numbers: ",odd_numbers)
