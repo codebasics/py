65a6b504691351f7a4dac6844203fe0f2ef9547d
Karandeep Grover <karan@cloint.com>
2021-01-25 17:23:08 +0530

    formatted

--- Basics/Hindi/23_sets_frozensets/23_sets_frozensets.py
+++ Basics/Hindi/23_sets_frozensets/23_sets_frozensets.py
@@ -1,5 +1,5 @@
-set1 = {1,2,3,4,5}
-set2 = {4,5,6,7,8}
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
 print("Original sets:")
 print(set1)
 print(set2)
@@ -8,6 +8,6 @@ print(set1.difference(set2))
 print("Difference of set2 and set1 using difference():")
 print(set2.difference(set1))
 print("Difference of set1 and set2 using - operator:")
-print(set1-set2)
+print(set1 - set2)
 print("Difference of set2 and set1 using - operator:")
-print(set2-set1)
+print(set2 - set1)
