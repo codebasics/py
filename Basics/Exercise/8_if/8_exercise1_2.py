96f4e594641a69b2bd9d994428b09105ba9677e2
Maqsat <146412142+QamalMaqsat@users.noreply.github.com>
2023-10-04 18:21:14 +0600

    Update 8_exercise1_2.py

--- Basics/Exercise/8_if/8_exercise1_2.py
+++ Basics/Exercise/8_if/8_exercise1_2.py
@@ -6,19 +6,17 @@
 #     bangladesh = ["dhaka", "khulna", "rangpur"]
 #     ```
 #Write a program that asks user to enter two cities and it tells you if they both are in same country or not. For example if I enter mumbai and chennai, it will print "Both cities are in India" but if I enter mumbai and dhaka it should print "They don't belong to same country"
+Kazakhstan = ['Astana','Shymkent','Almaty','Aktau','Oral']
+America = ['New-York','Washington','Texas']
+Russia = ['Moscow','Sankt-peterburgs','Omsk','Chelyabinsk']
 
-india = ["mumbai", "banglore", "chennai", "delhi"]
-pakistan = ["lahore", "karachi", "islamabad"]
-bangladesh = ["dhaka", "khulna", "rangpur"]
-
-city1 = input("Enter city 1: ")
-city2 = input("Enter city 2: ")
-
-if city1 in india and city2 in india:
-    print("Both cities are in india")
-elif city1 in pakistan and city2 in pakistan:
-    print("Both cities are in pakistan")
-elif city1 in bangladesh and city2 in bangladesh:
-    print("Both cities are in bangladesh")
+city = input('enter your city:')
+city1 = input('Enter your city 2')
+if city in Kazakhstan and city1 in Kazakhstan:
+    print('Kazakhstan')
+elif city in America and city1 in America:
+    print('America')
+elif city in Russia and city1 in Russia:
+    print('Russia')
 else:
-    print("They don't belong to same country")
+    print('they dont belong to same country', city)
