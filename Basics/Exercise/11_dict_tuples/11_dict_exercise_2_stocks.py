97485039d34e807601e338f1fde048be57a1491f
Maqsat <146412142+QamalMaqsat@users.noreply.github.com>
2023-10-06 11:04:44 +0600

    Update 11_dict_exercise_2_stocks.py

--- Basics/Exercise/11_dict_tuples/11_dict_exercise_2_stocks.py
+++ Basics/Exercise/11_dict_tuples/11_dict_exercise_2_stocks.py
@@ -1,36 +1,30 @@
-import statistics
-
-stocks = {
-    'info': [600,630,620],
-    'ril': [1430,1490,1567],
-    'mtl': [234,180,160]
+full = {
+    'info' :[600, 630, 620],
+    'ril' : [1430, 1490, 1567],
+    'mtl' : [234, 180, 160]
 }
 
-def print_all():
-    for stock,price_list in stocks.items():
-        avg = statistics.mean(price_list)
-        print(f"{stock} ==> {price_list} ==> avg: ",round(avg,2))
+def full_list():
+    for key, i in full.items():
+        print(f"{key}==>{i}")
 
-
-def add():
-    s = input("Enter a stock ticker to add:")
-    p = input("Enter price of this stock:")
-    p=float(p)
-    if s in stocks:
-        stocks[s].append(p)
+def add_list():
+    stock = input('stock')
+    price = (int(input('price')))
+    if stock in full:
+        full[stock].append(price)
     else:
-        stocks[s] = [p]
-    print_all()
+        full[stock] = [price]
+    full_list()
 
-
-def main():
-    op=input("Enter operation (print, add or amend):")
+def main ():
+    op = input('take operation :(print,add or amend?)')
     if op.lower() == 'print':
-        print_all()
+        full_list()
     elif op.lower() == 'add':
-        add()
+        add_list()
     else:
-        print("Unsupported operation:",op)
+        print('Unsupported operation:', op)
 
 if __name__ == '__main__':
     main()
