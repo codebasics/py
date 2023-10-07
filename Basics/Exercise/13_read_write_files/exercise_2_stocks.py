3263ed312423674bcaec8e32c912d63de6d043c3
Maqsat <146412142+QamalMaqsat@users.noreply.github.com>
2023-10-07 11:04:52 +0600

    Update exercise_2_stocks.py

--- Basics/Exercise/13_read_write_files/exercise_2_stocks.py
+++ Basics/Exercise/13_read_write_files/exercise_2_stocks.py
@@ -1,12 +1,12 @@
-with open("stocks.csv", "r") as f, open("output.csv", "w") as out:
-    out.write("Company Name,PE Ratio, PB Ratio\n")
-    next(f)  # This will skip first line in the file which is a header
+with open('C://Users//77081//PycharmProjects//Data//stocks.csv','r') as f, open('C://Users//77081//PycharmProjects//Data//output.csv','w') as out:
+    out.write('Company name, PE ratiom,PB ration\n')
+    next(f)
     for line in f:
-        tokens = line.split(",")
+        tokens = line.split(',')
         stock = tokens[0]
         price = float(tokens[1])
         eps = float(tokens[2])
         book = float(tokens[3])
-        pe = round(price / eps, 2)
-        pb = round(price / book, 2)
-        out.write(f"{stock},{pe},{pb}\n")
+        pe = round(price/eps, 2)
+        pb = round(price/book, 2)
+        out.write(f'{stock},{pe},{pb}\n')
