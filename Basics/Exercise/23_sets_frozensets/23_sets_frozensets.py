7bfe0ecb1306d2ae9c5d2f628a855bd54587f1c9
Maqsat <146412142+QamalMaqsat@users.noreply.github.com>
2023-10-26 05:19:48 +0600

    Update 23_sets_frozensets.py

--- Basics/Exercise/23_sets_frozensets/23_sets_frozensets.py
+++ Basics/Exercise/23_sets_frozensets/23_sets_frozensets.py
@@ -1,13 +1,11 @@
 set1 = {1, 2, 3, 4, 5}
 set2 = {4, 5, 6, 7, 8}
-print("Original sets:")
-print(set1)
-print(set2)
-print("Difference of set1 and set2 using difference():")
+
 print(set1.difference(set2))
-print("Difference of set2 and set1 using difference():")
 print(set2.difference(set1))
-print("Difference of set1 and set2 using - operator:")
+
+print("Разница сет 1 от сет2")
 print(set1 - set2)
-print("Difference of set2 and set1 using - operator:")
+
+print("Разница сет2 от сет1")
 print(set2 - set1)
